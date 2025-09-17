1de297883ca9bf004b36132a7c244aa7b316bd17
unknown <kdifallah@gamingcampus.local>
2025-09-17 14:19:08 +0200

    Initial Commit

---
+++ Source/TheHeist/TheHeist.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheHeist.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheHeist, "TheHeist" );
+
+DEFINE_LOG_CATEGORY(LogTheHeist)
---
+++ Source/TheHeist/TheHeistGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheHeistGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class ATheHeistGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATheHeistGameMode();
+};
+
+
+
