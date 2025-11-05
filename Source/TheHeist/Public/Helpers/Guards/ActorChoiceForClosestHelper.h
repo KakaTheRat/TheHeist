fd5f5648577a2c8c9faa89d5c1504584bf8347ab
CantineauFrancois <cantineaufrancois@hotmail.com>
2025-11-05 14:28:38 +0100

    add : squad single guard movements

---
+++ Source/TheHeist/Public/Helpers/Guards/ActorChoiceForClosestHelper.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ActorChoiceForClosestHelper.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEHEIST_API UActorChoiceForClosestHelper : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public :
+	
+	
+};
