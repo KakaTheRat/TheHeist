358083fdba929339005823ba88e5423b86bb86c1
Euexia <210350433+LucieQuintanaNT@users.noreply.github.com>
2025-11-05 16:11:18 +0100

    Add Logic Security Camera

---
+++ Source/TheHeist/Private/SecurityCamera.cpp
@@ -0,0 +1,29 @@
+﻿// C++
+#include "SecurityCamera.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Math/UnrealMathUtility.h"
+
+ASecurityCamera::ASecurityCamera()
+{
+    PrimaryActorTick.bCanEverTick = false; 
+    
+    AutoPossessPlayer = EAutoReceiveInput::Disabled;
+    bIsActive = true;
+}
+
+void ASecurityCamera::BeginPlay()
+{
+    Super::BeginPlay();
+
+    BaseRotation = InitialRotation + GetActorRotation();
+}
+
+void ASecurityCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void ASecurityCamera::SetActive(bool bEnable)
+{
+    bIsActive = bEnable;
+}
---
+++ Source/TheHeist/Public/SecurityCamera.h
@@ -0,0 +1,59 @@
+﻿// C++
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/RotatingMovementComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Curves/CurveFloat.h"
+#include "Camera/CameraComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "SecurityCamera.generated.h"
+
+UENUM(BlueprintType)
+enum class ECameraSweepMode : uint8
+{
+    None     UMETA(DisplayName = "None"),
+    PingPong UMETA(DisplayName = "PingPong"),
+    Constant UMETA(DisplayName = "Constant")
+};
+
+UCLASS(Blueprintable)
+class THEHEIST_API ASecurityCamera : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    ASecurityCamera();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+    ECameraSweepMode SweepMode = ECameraSweepMode::None;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta = (EditCondition = "SweepMode != ECameraSweepMode::None"))
+    float SweepAngle = 45.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta = (EditCondition = "SweepMode != ECameraSweepMode::None"))
+    float SweepSpeed = 30.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta = (EditCondition = "SweepMode == ECameraSweepMode::PingPong"))
+    UCurveFloat* SweepCurve;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+    FRotator InitialRotation = FRotator::ZeroRotator;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+    bool bIsActive = true;
+
+    UFUNCTION(BlueprintCallable, Category = "Camera")
+    void SetActive(bool bEnable);
+
+private:
+    FRotator BaseRotation;
+};
