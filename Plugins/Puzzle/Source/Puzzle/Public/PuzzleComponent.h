83bdcb7a46a4add31604b0d0c9b59b66b4bde8bd
Euexia <lucie.quintana04@gmail.com>
2025-12-01 17:35:25 +0100

    Add Plugins Puzzle

---
+++ Plugins/Puzzle/Source/Puzzle/Public/PuzzleComponent.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "PuzzleComponent.generated.h"
+
+UCLASS()
+class PUZZLE_API UPuzzleComponent : public UObject
+{
+	GENERATED_BODY()
+};
