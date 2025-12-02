e641523451f7d45bf077a5d8344bef859c3d8331
Euexia <lucie.quintana04@gmail.com>
2025-12-02 18:01:03 +0100

    Add Module KeyPad

--- Plugins/Puzzle/Source/Puzzle/Private/PuzzleComponent.cpp
+++ Plugins/Puzzle/Source/Puzzle/Private/PuzzleComponent.cpp
@@ -1,1 +1,26 @@
 ﻿#include "PuzzleComponent.h"
+
+UPuzzleComponent::UPuzzleComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+bool UPuzzleComponent::TrySolveEnigma(const FString& PlayerSolution)
+{
+	if (IsCompleted == true or EnigmaSolution == PlayerSolution)
+	{
+		IsCompleted = true;
+		return true;
+	}
+	return false;
+}
+
+bool UPuzzleComponent::NeedToCheckSolution(const FString& PlayerSolution) const
+{
+	if (PlayerSolution.Len() == EnigmaSolution.Len())
+	{
+		return true;
+	}
+	return false;
+}
+
--- Plugins/Puzzle/Source/Puzzle/Public/PuzzleComponent.h
+++ Plugins/Puzzle/Source/Puzzle/Public/PuzzleComponent.h
@@ -1,13 +1,26 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Object.h"
+#include "Components/ActorComponent.h"
+
 #include "PuzzleComponent.generated.h"
 
 UCLASS()
-class PUZZLE_API UPuzzleComponent : public UObject
+class PUZZLE_API UPuzzleComponent : public UActorComponent
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (AllowPrivateAccess = "true") )
+	FString EnigmaSolution = "0";
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true") )
+	bool IsCompleted;
+
+public:
+	// Sets default values for this component's properties
+	UPuzzleComponent();
+
+	bool TrySolveEnigma(const FString& PlayerSolution);
+	bool NeedToCheckSolution(const FString& PlayerSolution) const;
 };
