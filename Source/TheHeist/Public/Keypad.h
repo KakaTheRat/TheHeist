15276c5e82ce5c2962e9a5302c9e481cd5b12d5b
KakaTheReal <147564595+KakaTheRat@users.noreply.github.com>
2025-12-02 17:00:46 +0100

    KeypadV2Start

---
+++ Source/TheHeist/Public/Keypad.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EnigmaComponent.h"
+#include "Components/TextRenderComponent.h" // Important pour le texte 3D
+#include "Keypad.generated.h"
+
+UCLASS()
+class THEHEIST_API APhysicalKeypad : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	APhysicalKeypad();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UEnigmaComponent* EnigmaComponent;
+
+	// Le composant qui affiche le texte en 3D dans le monde
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UTextRenderComponent* ScreenText;
+
+	// Mémorise ce que le joueur tape
+	FString CurrentCode;
+
+public:	
+	// Appelée par les boutons
+	void ReceiveInput(FString Value);
+
+private:
+	void UpdateDisplay();
+	void HandleValidation();
+};
