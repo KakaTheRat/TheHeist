f032aecd22bd27707a61ee0b937c63496afcc74e
Euexia <lucie.quintana04@gmail.com>
2025-12-02 18:15:12 +0100

    Add Logic KeyPad

---
+++ Plugins/Puzzle/Source/PuzzleKeyPad/Private/PuzzleKeyPadButton.cpp
@@ -0,0 +1,19 @@
+﻿#include "PuzzleKeyPadButton.h"
+
+#include "PuzzleKeyPad.h"
+
+APuzzleKeyPadButton::APuzzleKeyPadButton()
+{
+	ButtonMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ButtonMesh"));
+	RootComponent = ButtonMesh;
+}
+
+void APuzzleKeyPadButton::InterractWithButton()
+{
+	if (LinkedKeypad)
+	{
+		// On appuie physiquement sur le bouton (petite animation optionnelle)
+		// Puis on envoie la valeur au Keypad central
+		LinkedKeypad->ReceiveInput(ButtonValue);
+	}
+}
