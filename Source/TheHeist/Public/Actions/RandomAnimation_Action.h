0b38e609ad7108140ed1e650cb26b4dee45db9c0
CantineauFrancois <cantineaufrancois@hotmail.com>
2025-12-02 16:56:30 +0100

    add : npcs actions

---
+++ Source/TheHeist/Public/Actions/RandomAnimation_Action.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/Action_Base.h"
+#include "RandomAnimation_Action.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEHEIST_API URandomAnimation_Action : public UAction_Base
+{
+	GENERATED_BODY()
+	
+};
