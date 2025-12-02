de9a73b0bff190dbe49295b72c0c99edf0e3dc79
CantineauFrancois <cantineaufrancois@hotmail.com>
2025-12-02 16:56:30 +0100

    add : npcs actions

---
+++ Source/TheHeist/Private/Actions/Talk_Action.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actions/Talk_Action.h"
+
+void UTalk_Action::OnExecute_Implementation(AActor* OwningGuard)
+{
+	
+}
