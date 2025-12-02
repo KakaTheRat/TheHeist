b53d97f97c888f633ed2015945d2e8bac8b535fe
Euexia <lucie.quintana04@gmail.com>
2025-12-02 18:15:12 +0100

    Add Logic KeyPad

---
+++ Plugins/Puzzle/Source/PuzzleKeyPad/Private/PuzzleKeyPad.cpp
@@ -0,0 +1,81 @@
+﻿#include "PuzzleKeyPad.h"
+
+
+APuzzleKeyPad::APuzzleKeyPad()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+    // Création d'un mesh de base pour le boitier (optionnel)
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	PuzzleComponent = CreateDefaultSubobject<UPuzzleComponent>(TEXT("PuzzleComponent"));
+    
+    // Configuration de l'écran texte 3D
+	ScreenText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("ScreenText"));
+	ScreenText->SetupAttachment(RootComponent);
+    ScreenText->SetHorizontalAlignment(EHTA_Center);
+    ScreenText->SetText(FText::FromString("----"));
+    ScreenText->SetWorldSize(20.0f); // Taille du texte
+}
+
+void APuzzleKeyPad::BeginPlay()
+{
+	Super::BeginPlay();
+	CurrentCode = "";
+    UpdateDisplay();
+}
+
+void APuzzleKeyPad::ReceiveInput(FString Value)
+{
+    // Si on veut une touche "Clear" ou "Reset"
+    if(Value == "C") 
+    {
+        CurrentCode = "";
+        UpdateDisplay();
+        return;
+    }
+
+    // Ajout du chiffre
+	CurrentCode.Append(Value);
+	UpdateDisplay();
+
+    // Vérification via votre component existant
+	if (PuzzleComponent && PuzzleComponent->NeedToCheckSolution(CurrentCode))
+	{
+		HandleValidation();
+	}
+}
+
+void APuzzleKeyPad::UpdateDisplay()
+{
+    if(ScreenText)
+    {
+        ScreenText->SetText(FText::FromString(CurrentCode));
+    }
+}
+
+void APuzzleKeyPad::HandleValidation()
+{
+	bool bSuccess = PuzzleComponent->TrySolveEnigma(CurrentCode);
+
+	if (bSuccess)
+	{
+		ScreenText->SetTextRenderColor(FColor::Green);
+		ScreenText->SetText(FText::FromString("OPEN"));
+        // TODO : Ouvrir la porte ici ou appeler un Event
+	}
+	else
+	{
+		ScreenText->SetTextRenderColor(FColor::Red);
+		ScreenText->SetText(FText::FromString("ERR"));
+        
+        // Timer pour reset après 1 seconde (Lambda function)
+        FTimerHandle UnusedHandle;
+        GetWorldTimerManager().SetTimer(UnusedHandle, [this]()
+        {
+            CurrentCode = "";
+            ScreenText->SetTextRenderColor(FColor::White); // Remet en blanc
+            UpdateDisplay();
+        }, 1.5f, false);
+	}
+}
---
+++ Plugins/Puzzle/Source/PuzzleKeyPad/Public/PuzzleKeyPad.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "PuzzleComponent.h"
+#include "Components/TextRenderComponent.h"
+
+#include "PuzzleKeyPad.generated.h"
+
+UCLASS()
+class PUZZLEKEYPAD_API APuzzleKeyPad : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UPuzzleComponent* PuzzleComponent;
+
+	// Le composant qui affiche le texte en 3D dans le monde
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UTextRenderComponent* ScreenText;
+
+	// Mémorise ce que le joueur tape
+	FString CurrentCode;
+
+public:	
+	// Appelée par les boutons
+	void ReceiveInput(FString Value);
+
+private:
+	void UpdateDisplay();
+	void HandleValidation();
+};
